7ad75073152bb591b4734c3d78d67febdec23b9a
alextern0 <alexandr.ternovsky@gmail.com>
2022-12-19 18:45:55 +0300

    добавил виджет прогноза погоды

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,6 @@
     <div class="footer-wrapper">
     <div class="footer-left">
         <div class="house">MyHouse</div>
-
     </div>
     <div class="footer-center">
         <a href="#">О нас</a><br><br>
@@ -16,5 +15,7 @@
         <a href="#">Время работы: 10:00 - 20:00</a><br><br>
         <a href="#">E-mail: vip@housevip.ru</a><br><br>
     </div>
+        <div class="weather"><!-- weather widget start --><a target="_blank" href="https://nochi.com/weather/pavlovskoye-445200"><img src="https://w.bookcdn.com/weather/picture/1_445200_1_20_137AE9_160_ffffff_333333_08488D_1_ffffff_333333_0_6.png?scode=&domid=589&anc_id=37664"  alt="booked.net"/></a><!-- weather widget end --></div>
+
     </div>
 </footer>
