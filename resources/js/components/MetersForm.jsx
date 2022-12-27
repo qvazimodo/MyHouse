f3fefdba3b88e077fc33ad856f8a3327c4485205
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2022-12-27 15:36:50 +0300

    Месяц не передаётся в БД.

--- resources/js/components/MetersForm.jsx
+++ resources/js/components/MetersForm.jsx
@@ -11,7 +11,7 @@ class MetersForm extends React.Component {
         last: '',
         now: '',
         userId: '',
-        month:''
+        month: '',
     };
 
     componentDidMount() {
@@ -34,135 +34,139 @@ class MetersForm extends React.Component {
             body: JSON.stringify( {
                 number: this.state.number,
                 user_id: this.state.userId,
+                month: this.state.month,
                 value: this.state.now,
                 // type: this.state.name,
                 type: this.state.type,
                 // type: 'hot_water',
-                month: 5,
-            })
-        })
-            .then(response => response.json())
-            .then(data => console.log(data))
 
-        alert("Данные приняты");
+            } )
+        } )
+            .then( response => response.json() )
+            .then( data => console.log( data ) )
+
+        alert( "Данные приняты" );
 
     }
-    typeChange = (value) => {
-        this.setState({type: value});
-        console.log(this.state);
+    typeChange = ( value ) => {
+        this.setState( { type: value } );
     }
-    numberChange = (e) => {
-        this.setState({number: e.target.value});
+    numberChange = ( e ) => {
+        this.setState( { number: e.target.value } );
     }
-    lastChange = (e) => {
-        this.setState({last: e.target.value});
+    lastChange = ( e ) => {
+        this.setState( { last: e.target.value } );
     }
-    nowChange = (e) => {
-        this.setState({now: e.target.value});
+    nowChange = ( e ) => {
+        this.setState( { now: e.target.value } );
     }
-    monthChange = (e) => {
-        this.setState({month: value});
+    monthChange = ( value ) => {
+        this.setState( { month: value } );
+        console.log( value );
+        console.log( this.state );
+    }
+    typeChange = ( value ) => {
+        this.setState( { type: value } );
     }
 
-render() {
-    return (
-        <div className="container">
-            <Collapse accordion>
-                <Panel header="Ввести показания счетчиков" key="1" className="cabinet-txt">
-                    <Form>
-                        <Form.Item label="Выберите счетчик">
-                            <Select
-                                // defaultValue="cold_water"
-                                onChange={this.typeChange}
-                                options={[
-                                    {
-                                        value: 'cold_water',
-                                        label: 'Счетчик холодной воды',
-                                    },
-                                    {
-                                        value: 'hot_water',
-                                        label: 'Счетчик горячей воды',
-                                    },
-                                    {
-                                        value: 'heat',
-                                        label: 'Счетчик газа',
-                                    },
-                                    {
-                                        value: 'electricity',
-                                        label: 'Счетчик электричества',
-                                    },
-                                ] }
-                            />
-                        </Form.Item>
-                        <Form.Item label="Заводской номер счетчика">
-                            <Input value={ this.state.number } onChange={ this.numberChange }/>
-                        </Form.Item>
-                        <Form.Item label="Показания за прошлый период">
-                            <Input value={ this.state.last } onChange={ this.lastChange }/>
-                        </Form.Item>
-                        <Form.Item label="Выберите месяц">
-                            <Select
-                                // defaultValue="cold_water"
-                                onChange={ this.monthChange }
-                                options={ [
-                                    {
-                                        value: 1,
-                                        label: 'январь',
-                                    },
-                                    {
-                                        value: 2,
-                                        label: 'февраль',
-                                    },
-                                    {
-                                        value: 3,
-                                        label: 'март',
-                                    },
-                                    {
-                                        value: 4,
-                                        label: 'апрель',
-                                    }, {
-                                        value: 5,
-                                        label: 'май',
-                                    }, {
-                                        value: 6,
-                                        label: 'июнь',
-                                    }, {
-                                        value: 7,
-                                        label: 'июль',
-                                    }, {
-                                        value: 8,
-                                        label: 'август',
-                                    },
-                                    {
-                                        value: 9,
-                                        label: 'сентябрь',
-                                    },
-                                    {
-                                        value: 10,
-                                        label: 'октябрь',
-                                    },
-                                    {
-                                        value: 11,
-                                        label: 'ноябрь',
-                                    },
-                                    {
-                                        value: 12,
-                                        label: 'декабрь',
-                                    },
-                                ] }
-                            />
-                        </Form.Item>
+    render() {
+        return (
+            <div className="container">
+                <Collapse accordion>
+                    <Panel header="Ввести показания счетчиков" key="1" className="cabinet-txt">
+                        <Form>
+                            <Form.Item label="Выберите счетчик">
+                                <Select
+                                    // defaultValue="cold_water"
+                                    onChange={ this.typeChange }
+                                    options={ [
+                                        {
+                                            value: 'cold_water',
+                                            label: 'Счетчик холодной воды',
+                                        },
+                                        {
+                                            value: 'hot_water',
+                                            label: 'Счетчик горячей воды',
+                                        },
+                                        {
+                                            value: 'heat',
+                                            label: 'Счетчик газа',
+                                        },
+                                        {
+                                            value: 'electricity',
+                                            label: 'Счетчик электричества',
+                                        },
+                                    ] }
+                                />
+                            </Form.Item>
+                            <Form.Item label="Заводской номер счетчика">
+                                <Input value={ this.state.number } onChange={ this.numberChange }/>
+                            </Form.Item>
+                            <Form.Item label="Показания за прошлый период">
+                                <Input value={ this.state.last } onChange={ this.lastChange } disabled/>
+                            </Form.Item>
+                            <Form.Item label="Выберите месяц">
+                                <Select
+                                    onChange={ this.monthChange }
+                                    options={ [
+                                        {
+                                            value: 1,
+                                            label: 'январь',
+                                        },
+                                        {
+                                            value: 2,
+                                            label: 'февраль',
+                                        },
+                                        {
+                                            value: 3,
+                                            label: 'март',
+                                        },
+                                        {
+                                            value: 4,
+                                            label: 'апрель',
+                                        }, {
+                                            value: 5,
+                                            label: 'май',
+                                        }, {
+                                            value: 6,
+                                            label: 'июнь',
+                                        }, {
+                                            value: 7,
+                                            label: 'июль',
+                                        }, {
+                                            value: 8,
+                                            label: 'август',
+                                        },
+                                        {
+                                            value: 9,
+                                            label: 'сентябрь',
+                                        },
+                                        {
+                                            value: 10,
+                                            label: 'октябрь',
+                                        },
+                                        {
+                                            value: 11,
+                                            label: 'ноябрь',
+                                        },
+                                        {
+                                            value: 12,
+                                            label: 'декабрь',
+                                        },
+                                    ] }
+                                />
+                            </Form.Item>
                             <Form.Item label="Текущие показания">
                                 <Input value={ this.state.now } onChange={ this.nowChange }/>
                             </Form.Item>
                             <Form.Item>
                                 <Button type="primary" onClick={ this.sendForm }>Отправить</Button>
                             </Form.Item>
-                    </Form>
-                </Panel>
-            </Collapse>
+                        </Form>
+                    </Panel>
+                </Collapse>
 
-        </div>
+            </div>
     );
 }
 }
