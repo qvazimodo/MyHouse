e76b2406e23f0eab0183b1b798ce3638cab3f19e
Denis_Davidov <redman1922svap@mail.ru>
2022-12-28 20:42:32 +0300

    Сделал список товаров на тестовой странице значения подтягиваются из json

--- resources/js/components/ListCards.jsx
+++ resources/js/components/ListCards.jsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import ListCard from "./ListCard";
+import Preloader from "./Preloader";
+import {Card} from "antd";
 
-function ListCards() {
+const ListCards = () => {
+
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+    let cardList = [];
+
+
+    useEffect(() => {
+        fetch('http://127.0.0.1/api/cards/')
+            .then(response => {
+                //получаем данные из json
+                if (response.ok) {
+                    return response.json()
+                }
+                throw response;
+            })
+
+            .then(data => {
+                //выбираем из json значения для карточек
+                for (let i in data){
+                    for (let j in data[i].card){
+                        cardList.push(data[i].card[j])
+                    }
+                }
+                setItems(cardList)
+                //флаг для кружка загрузки
+                setLoading(false)
+            });
+    }, [])
+
     return (
-        <div>test1234</div>
+        <Card title="Список Товаров">
+            {loading ? (
+                <Preloader/>
+            ) : items.length ? (
+                items.map(item => (
+                    //прокидываем значения в ListCard.jsx
+                    <ListCard key={item.id} {...item} />
+                ))
+            ) : (
+                <p>Не удалось загрузить список</p>
+            )}
+        </Card>
     );
 }
 
---
+++ resources/js/components/Preloader.jsx
@@ -0,0 +1,7 @@
+const Preloader = () => {
+    return (
+        <div className="preloader-5"></div>
+    );
+}
+
+export default Preloader;
