14c35ee59d768e7e7bbb7cbbe93dc4605409e541
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-01-01 19:52:59 +0300

    Проверено отношение один к одному между моделями User и Employee.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 //use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
@@ -50,7 +51,7 @@ class User extends Authenticatable
         return $this->hasOne(Client::class);
     }
 
-    public function employee()
+    public function employee():HasOne
     {
         return $this->hasOne(Employee::class);
     }
