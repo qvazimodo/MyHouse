c553ec00ffc772281705d3afce168ff689ba6016
MAG1980 <al-1980@inbox.ru>
2023-01-02 16:56:31 +0300

    Засеяна таблица сотрудников организации.

--- database/seeders/ClientsSeeder.php
+++ database/seeders/ClientsSeeder.php
@@ -23,27 +23,33 @@ class ClientsSeeder extends Seeder
      */
     public function run()
     {
-        Client::factory(3)
-            ->has(Card::factory(3)
-                ->has(Photo::factory(3)))
-            ->create();
-        Client::factory(2)                      //количество клиентов
-            ->has(Meter::factory(2)             //количество счётчиков у клиента
-                ->has(MeterValue::factory()
-                    ->state(new Sequence(
-                        ['month_id' => 1],
-                        ['month_id' => 2],
-                        ['month_id' => 3],
-                    ))
-                    ->afterCreating(function (MeterValue $meterValue) {
-                        if ($meterValue['id'] !== 1) {
-                            $meterValue['parent_id'] = $meterValue['id'] - 1;
-                            $meterValue->save();
-                        }
-
-                        var_dump($meterValue['id']);
-                    })->count(3)                //количество показаний счётчика
-                ))
+        Client::factory(2)              //количество клиентов
+        ->has(Card::factory(3)          //количество объявлений у клиента
+        ->has(Photo::factory(3)))       //количество фотографий у объявления
+        ->has(Meter::factory(2)         //количество счётчиков у клиента
+        ->has(MeterValue::factory()
+            ->count(24)                 //количество показаний у каждого счётчика (кратно 12)
+            ->state(new Sequence(
+                ['month_id' => 1],
+                ['month_id' => 2],
+                ['month_id' => 3],
+                ['month_id' => 4],
+                ['month_id' => 5],
+                ['month_id' => 6],
+                ['month_id' => 7],
+                ['month_id' => 8],
+                ['month_id' => 9],
+                ['month_id' => 10],
+                ['month_id' => 11],
+                ['month_id' => 12],
+            ))
+            ->afterCreating(function (MeterValue $meterValue) {
+                if ($meterValue['id'] !== 1) {
+                    $meterValue['parent_id'] = $meterValue['id'] - 1;
+                    $meterValue->save();
+                }
+            })
+        ))
             ->create();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call(MonthSeeder::class);
         $this->call(UsersSeeder::class);
         $this->call(ClientsSeeder::class);
-//        $this->call(EmployeesSeeder::class);
+        $this->call(EmployeesSeeder::class);
 //        $this->call(CardsSeeder::class);
 
 //        $this->call(MeterSeeder::class);
