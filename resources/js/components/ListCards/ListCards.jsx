c4d659c2036a6466aa689ff88b737defb7d18b7f
Denis_Davidov <redman1922svap@mail.ru>
2023-01-09 18:28:31 +0300

    update Form

--- resources/js/components/ListCards/ListCards.jsx
+++ resources/js/components/ListCards/ListCards.jsx
@@ -4,18 +4,8 @@ import {CARDS_API_URL, EMPLOYEES_API_URL} from "../../helpers/API";
 import ColumnGroup from "antd/es/table/ColumnGroup";
 import Column from "antd/es/table/Column";
 
-const {Text} = Typography;
-
 
 const ListCards = () => {
-
-    const [items, setItems] = useState([]);
-    const [argument, setArgument] = useState({
-        userId: ''
-    });
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
 
     //-----------------------------------
     const [employeesList, setEmployeesList] = useState([])
@@ -64,73 +54,8 @@ const ListCards = () => {
             });
     }
 
-
-    //----------------------------------------------------
-    useEffect(() => {
-        fetch('http://localhost/api/auth_user')
-            .then(response => response.json())
-            .catch(e => console.log(e))
-            .then(data => setArgument({
-                userId: data.id
-            }));
-
-        /* Получение информации по счетчикам пользователя */
-
-    }, [])
-
-    const sendForm = (e) => {
-        if (argument.userId === '101') {
-            e.preventDefault();
-            fetch(`http://127.0.0.1/api/cards`, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                    // 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-                },
-                body: JSON.stringify({
-                    title: title,
-                    price: +price,
-                    description: description,
-                    user_id: +argument,
-                })
-            })
-                .then(response => response.json())
-                .catch(e => console.log(e))
-                .then(data => {
-                    setItems([...items, data])
-                })
-        } else {
-            window.alert('У вас нет прав')
-        }
-    }
-
-
     return (
         <div className="container">
-            <Text style={{color: '#D4C17F', marginBottom: 30, display: 'block', fontSize: 18}}>Заполните форму для ввода новых показаний:</Text>
-            <Form>
-                <Form.Item label="Введите заголовок:">
-                    <Input name='title' value={title} onChange={(e) => {
-                        setTitle(e.target.value)
-                    }}/>
-                </Form.Item>
-                <Form.Item label="Введите описание:">
-                    <Input name='description' value={description} onChange={(e) => {
-                        setDescription(e.target.value)
-                    }}/>
-                </Form.Item>
-                <Form.Item label="Введите цену:">
-                    <Input name='price' value={price} type='number' onChange={(e) => {
-                        setPrice(e.target.value)
-                    }}/>
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" onClick={sendForm}>Отправить</Button>
-                </Form.Item>
-            </Form>
-
             <List
                 grid={{
                     gutter: 16,
--- resources/js/components/UserCards.jsx
+++ resources/js/components/UserCards.jsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {Card, List} from "antd";
+import {Button, Card, Form, Input, List, Typography} from "antd";
 import {MYCARDS_API_URL} from "../helpers/API";
 
-
+const {Text} = Typography;
 
 const UserCards = () => {
 
@@ -13,6 +13,18 @@ const UserCards = () => {
     const [data, setData] = useState([]);
     const [links, setLinks] = useState({});
     const [current, setCurrent] = useState(1);
+
+    //--------------Для формы хуки
+    const [items, setItems] = useState([]);
+    const [argument, setArgument] = useState({
+        userId: ''
+    });
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+
+    //---------------------- Для Кости (НЕ форма)
+    //---------------------- Вывод карточек пользователя
 
     const onPageChange = (page) => {
         fetchEmployees(MYCARDS_API_URL + `?page=${page}`)
@@ -51,11 +63,79 @@ const UserCards = () => {
             });
     }
 
+    //-----------------------------------Форма
+
+    useEffect(() => {
+        fetch('http://localhost/api/auth_user')
+            .then(response => response.json())
+            .catch(e => console.log(e))
+            .then(data => setArgument({
+                userId: data.id
+            }));
+
+        /* Получение информации по счетчикам пользователя */
+
+    }, [])
+
+
+    const sendForm = (e) => {
+
+            e.preventDefault();
+            fetch(`http://127.0.0.1/api/cards`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                    // 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                },
+                body: JSON.stringify({
+                    title: title,
+                    price: +price,
+                    description: description,
+                    client_id: +argument,
+                })
+            })
+                .then(response => response.json())
+                .catch(e => console.log(e))
+                .then(data => {
+                    console.log(data)
+                })
+
+    }
+
 
 
 
     return (
         <div className='container'>
+
+            {/*Форма*/}
+
+            <Text style={{color: '#D4C17F', marginBottom: 30, display: 'block', fontSize: 18}}>Заполните форму для ввода новых показаний:</Text>
+            <Form>
+                <Form.Item label="Введите заголовок:">
+                    <Input name='title' value={title} onChange={(e) => {
+                        setTitle(e.target.value)
+                    }}/>
+                </Form.Item>
+                <Form.Item label="Введите описание:">
+                    <Input name='description' value={description} onChange={(e) => {
+                        setDescription(e.target.value)
+                    }}/>
+                </Form.Item>
+                <Form.Item label="Введите цену:">
+                    <Input name='price' value={price} type='number' onChange={(e) => {
+                        setPrice(e.target.value)
+                    }}/>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" onClick={sendForm}>Отправить</Button>
+                </Form.Item>
+            </Form>
+
+            {/*Карточки*/}
+
             <h2 className='mycards'>Ваши карточки</h2>
             <List
                 grid={{
