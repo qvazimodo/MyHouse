bce18b8f95ab4b261e8d7500466162e9ba3f1399
Denis_Davidov <redman1922svap@mail.ru>
2023-01-09 20:43:18 +0300

    Update UserCards.jsx

--- resources/js/components/UserCards.jsx
+++ resources/js/components/UserCards.jsx
@@ -141,9 +141,9 @@ const UserCards = () => {
                     xs: 1,
                     sm: 2,
                     md: 2,
-                    lg: 3,
-                    xl: 3,
-                    xxl: 3,
+                    lg: 2,
+                    xl: 2,
+                    xxl: 2,
                 }}
                 pagination={ {
                     pageSize,
