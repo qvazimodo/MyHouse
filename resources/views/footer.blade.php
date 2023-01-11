ab9cb71e824bacce1b8e5ae57a07851733965613
alextern0 <86599134+alextern0@users.noreply.github.com>
2023-01-11 12:50:56 +0300

    удалил блок footer-center и стилизацию к нему

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -12,12 +12,12 @@
         <a href="#">Тарифы и услуги</a><br><br>
         <a href="contacts">Контакты</a>
     </div>
-    <div class="footer-right">
-        Адрес: Наб. реки Фонтанки 10-15<br><br>
-        Телефон: 8 (812) 123-45-67<br><br>
-        Время работы: 10:00 - 20:00<br><br>
-        E-mail: vip@housevip.ru<br><br>
-    </div>
+{{--    <div class="footer-right">--}}
+{{--        Адрес: Наб. реки Фонтанки 10-15<br><br>--}}
+{{--        Телефон: 8 (812) 123-45-67<br><br>--}}
+{{--        Время работы: 10:00 - 20:00<br><br>--}}
+{{--        E-mail: vip@housevip.ru<br><br>--}}
+{{--    </div>--}}
         <div class="weather"><!-- weather widget start --><a target="_blank" href="https://nochi.com/weather/pavlovskoye-445200"><img src="https://w.bookcdn.com/weather/picture/1_445200_1_20_137AE9_160_ffffff_333333_08488D_1_ffffff_333333_0_6.png?scode=&domid=589&anc_id=37664"  alt="booked.net"/></a><!-- weather widget end --></div>
 
     </div>
