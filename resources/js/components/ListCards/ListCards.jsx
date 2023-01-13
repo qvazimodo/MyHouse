c266be2999ff036bf8359150d6ab01e51bca56cf
Denis_Davidov <redman1922svap@mail.ru>
2023-01-13 19:03:14 +0300

    Update ListCards

--- resources/js/components/ListCards/ListCards.jsx
+++ resources/js/components/ListCards/ListCards.jsx
@@ -1,5 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import {Avatar, Button, Card, Col, Form, Input, List, Row, Space, Table, Typography} from "antd";
+import {
+    Avatar,
+    Button,
+    Card,
+    Col,
+    ConfigProvider,
+    Form,
+    Image,
+    Input,
+    List,
+    Row,
+    Space,
+    Table,
+    theme,
+    Typography
+} from "antd";
 import {CARDS_API_URL, EMPLOYEES_API_URL} from "../../helpers/API";
 import ColumnGroup from "antd/es/table/ColumnGroup";
 import Column from "antd/es/table/Column";
@@ -16,6 +31,7 @@ const ListCards = () => {
     const [data, setData] = useState([]);
     const [links, setLinks] = useState({});
     const [current, setCurrent] = useState(1);
+    const [visible, setVisible] = useState(false);
 
 
     const onPageChange = (page) => {
@@ -56,34 +72,77 @@ const ListCards = () => {
 
     return (
         <div className="container">
-            <h2 className="title-2">Объявления</h2>
-            <List
-                style={{
-                    marginBottom: 30
+            <ConfigProvider
+                theme={{
+                    algorithm: theme.darkAlgorithm,
                 }}
-                grid={{
-                    gutter: 16,
-                    xs: 1,
-                    sm: 2,
-                    md: 2,
-                    lg: 3,
-                    xl: 3,
-                    xxl: 3,
-                }}
-                pagination={ {
-                    pageSize,
-                    total: totalPages,
-                    onChange: onPageChange
-                    // showSizeChanger: true,
-                } }
-                loading={ loading }
-                dataSource={data}
-                renderItem={(item) => (
-                    <List.Item>
-                        <Card title={`Заголовок: ${item.title} client_id: ${item.client_id}`}>Номер объявления: {item.key} <br/><br/> Текст: {item.description}<br/><br/>Цена: {item.price}$</Card>
-                    </List.Item>
-                )}
-            />
+
+            >
+                <h2 className="title-2">Объявления</h2>
+                <List
+                    style={{
+                        marginBottom: 30
+                    }}
+                    grid={{
+                        gutter: 16,
+                        xs: 1,
+                        sm: 2,
+                        md: 2,
+                        lg: 3,
+                        xl: 3,
+                        xxl: 3,
+                    }}
+                    pagination={{
+                        pageSize,
+                        total: totalPages,
+                        onChange: onPageChange
+                        // showSizeChanger: true,
+                    }}
+                    loading={loading}
+                    dataSource={data}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Card title={`Заголовок: ${item.title} client_id: ${item.client_id}`}>
+                                <div style={{textAlign:"center"}}>
+                                    <Image
+                                        preview={{
+                                            visible: false,
+                                        }}
+                                        width={200}
+                                        src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
+                                        onClick={() => setVisible(true)}
+                                    />
+                                    <div
+                                        style={{
+                                            display: 'none',
+                                        }}
+                                    >
+                                        <Image.PreviewGroup
+                                            preview={{
+                                                visible,
+                                                onVisibleChange: (vis) => setVisible(vis),
+                                            }}
+                                        >
+                                            {/*<div>{item.img.map((photopath) => <Image*/}
+                                            {/*    src={photopath.path}/>)}</div>*/}
+
+                                            <Image
+                                                src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"/>
+                                            <Image
+                                                src="https://gw.alipayobjects.com/zos/antfincdn/cV16ZqzMjW/photo-1473091540282-9b846e7965e3.webp"/>
+                                            <Image
+                                                src="https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp"/>
+
+                                        </Image.PreviewGroup>
+                                    </div>
+                                </div>
+                                <br/>
+                                Номер
+                                объявления: {item.key} <br/><br/> Текст: {item.description}<br/><br/>Цена: {item.price}$</Card>
+                        </List.Item>
+                    )}
+                />
+            </ConfigProvider>
         </div>
     );
 }
