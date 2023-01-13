9d3a0fc046ea08cfcf5b30fd411c3bb2e2f1158d
Denis_Davidov <redman1922svap@mail.ru>
2023-01-13 18:59:54 +0300

    update MyCards

--- resources/js/components/UserCards/UserCards.jsx
+++ resources/js/components/UserCards/UserCards.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Card, Form, Input, List, Typography} from "antd";
+import {Button, Card, ConfigProvider, Form, Image, Input, List, theme, Typography} from "antd";
 import {AUTH_USER_API_URL, CARDS_API_URL, MYCARDS_API_URL} from "../../helpers/API";
 import s from './UserCards.module.css';
 
@@ -24,6 +24,8 @@ const UserCards = () => {
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
 
+    const [visible, setVisible] = useState(false);
+
     //---------------------- Для Кости (НЕ форма)
     //---------------------- Вывод карточек пользователя
 
@@ -45,6 +47,7 @@ const UserCards = () => {
                 description: item.description,
                 price: item.price,
                 client_id: item.client_id,
+                img: item.photos
             }
         }))
     }, [employeesList]);
@@ -81,63 +84,71 @@ const UserCards = () => {
 
     const sendForm = (e) => {
 
-            e.preventDefault();
-            fetch(CARDS_API_URL, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-                },
-                body: JSON.stringify({
-                    title: title,
-                    price: +price,
-                    description: description,
-                    client_id: argument.userId,
-                })
+        e.preventDefault();
+        fetch(CARDS_API_URL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+            },
+            body: JSON.stringify({
+                title: title,
+                price: +price,
+                description: description,
+                client_id: argument.userId,
             })
-                .then(response => response.json())
-                .catch(e => console.log(e))
-                .then(result => {
-                    setData([...data,result])
-                    console.log(result)
-                })
+        })
+            .then(response => response.json())
+            .catch(e => console.log(e))
+            .then(result => {
+                setData([...data, result])
+                console.log(result)
+            })
 
     }
     console.log(data)
 
 
-
-
     return (
         <div className='container'>
+            <ConfigProvider
+                theme={{
+                    algorithm: theme.darkAlgorithm,
+                }}
+
+            >
 
             {/*Форма*/}
 
-            <Text style={{color: '#D4C17F', marginBottom: 30, display: 'block', fontSize: 18}}>Заполните форму для ввода новых показаний:</Text>
+            <Text style={{color: '#D4C17F', marginBottom: 30, marginTop: 65, display: 'block', fontSize: 24}}>Добавить объявление:</Text>
             <Form
                 layout={'vertical'}
                 size={'middle'}
             >
-                <Form.Item
-                    label="Введите заголовок:">
+                <span style={{fontSize:18, color:'#D4C17F'}}>Введите заголовок:</span>
+                <Form.Item>
                     <Input name='title' value={title} onChange={(e) => {
                         setTitle(e.target.value)
                     }}/>
                 </Form.Item>
-                <Form.Item label="Введите описание:">
+
+                <span style={{fontSize:18,color:'#D4C17F'}}>Введите описание:</span>
+                <Form.Item>
                     <Input name='description' value={description} onChange={(e) => {
                         setDescription(e.target.value)
                     }}/>
                 </Form.Item>
-                <Form.Item label="Введите цену:">
+
+                <span style={{fontSize:18, color:'#D4C17F'}}>Введите цену:</span>
+                <Form.Item>
                     <Input name='price' value={price} type='number' onChange={(e) => {
                         setPrice(e.target.value)
                     }}/>
                 </Form.Item>
                 <Form.Item>
-                    <Button type="primary" onClick={sendForm}>Отправить</Button>
+                    <Button type="primary" style={{backgroundColor:'#D4C17F'}} onClick={sendForm}>Отправить</Button>
                 </Form.Item>
             </Form>
 
@@ -154,20 +165,62 @@ const UserCards = () => {
                     xl: 2,
                     xxl: 2,
                 }}
-                pagination={ {
+                pagination={{
+                    style:{textAlign:'center'},
                     pageSize,
                     total: totalPages,
                     onChange: onPageChange
                     // showSizeChanger: true,
-                } }
-                loading={ loading }
+                }}
+                style={{marginBottom:90}}
+                loading={loading}
                 dataSource={data}
                 renderItem={(item) => (
                     <List.Item>
-                        <Card title={`Заголовок: ${item.title} client_id: ${item.client_id}`}>Номер объявления: {item.id} <br/><br/> Текст: {item.description}<br/><br/>Цена: {item.price}$</Card>
+                        <Card title={`Заголовок: ${item.title} client_id: ${item.client_id}`}>
+                            <div style={{textAlign:"center"}}>
+                                <Image
+                                    preview={{
+                                        visible: false,
+                                    }}
+                                    width={200}
+                                    src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
+                                    onClick={() => setVisible(true)}
+                                />
+                                <div
+                                    style={{
+                                        display: 'none',
+                                    }}
+                                >
+                                    <Image.PreviewGroup
+                                        preview={{
+                                            visible,
+                                            onVisibleChange: (vis) => setVisible(vis),
+                                        }}
+                                    >
+                                        {/*<div>{item.img.map((photopath) => <Image*/}
+                                        {/*    src={photopath.path}/>)}</div>*/}
+
+                                        <Image
+                                            src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"/>
+                                        <Image
+                                            src="https://gw.alipayobjects.com/zos/antfincdn/cV16ZqzMjW/photo-1473091540282-9b846e7965e3.webp"/>
+                                        <Image
+                                            src="https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp"/>
+
+                                    </Image.PreviewGroup>
+                                </div>
+                            </div>
+                            <br/>
+
+                            Номер объявления: {item.id} <br/><br/> Текст: {item.description}<br/><br/>Цена: {item.price}$
+
+                        </Card>
                     </List.Item>
+
                 )}
             />
+                </ConfigProvider>
         </div>
     );
 }
