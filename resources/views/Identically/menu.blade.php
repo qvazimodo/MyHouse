0bd142a04bc66356a59e153b2d31fd3ed7ac582c
alextern0 <86599134+alextern0@users.noreply.github.com>
2023-01-21 21:05:41 +0300

    Update

--- resources/views/Identically/menu.blade.php
+++ resources/views/Identically/menu.blade.php
@@ -14,40 +14,33 @@
                 @if (Route::has('login'))
                     <li><a href="{{ route('login') }}">Вход</a></li>
                 @endif
-        </ul>
         @else
             @if (Auth::user()->is_admin )
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                       aria-expanded="false">
-                        Панель администратора</a>
-                    <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="{{ route('admin.clients.index') }}">Клиенты</a></li>
                         <li><a class="dropdown-item" href="{{ route('admin.employees') }}">Сотрудники</a></li>
                         <li><a class="dropdown-item" href="{{ route('admin.meters') }}">Счётчики</a></li>
-                    </ul>
-                </li>
-
             @endif
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                   aria-expanded="false">
-                    {{ Auth::user()->name  }}</a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="{{ route('logout') }}"
+                        <li><a class="dropdown-item" href="{{ route('userProfile') }}">Мой профиль</a></li>
+                        <li><a class="dropdown-item" href="{{ route('usercards') }}">Мои объявления</a></li>
+
+
+                    <li>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
                            onclick="event.preventDefault();
-                           document.getElementById('logout-form').submit();"> Выход</a>
+                           document.getElementById('logout-form').submit();">
+                            Выход
+                        </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
-                        </form> </li>
-                    <li><a class="dropdown-item" href="{{ route('userProfile') }}">Мой профиль</a></li>
-                    <li><a class="dropdown-item" href="{{ route('usercards') }}">Мои объявления</a></li>
-                </ul>
-            </li>
+                        </form>
+                    </li>
+        </ul>
         @endguest
     </div>
-</div>
+ </div>
+<div class="user-name">
+    <div class="circle"></div>
+    {{ Auth::user()->name }}</div>
 
 
 
