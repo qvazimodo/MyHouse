9dd2f2b137b42e9636727ef3518b1e024210f207
MAG1980 <al-1980@inbox.ru>
2023-01-23 19:38:01 +0300

    Исправил опечатку в пути.

--- resources/js/helpers/API.js
+++ resources/js/helpers/API.js
@@ -8,7 +8,7 @@ export const EMPLOYEES_API_URL = DOMEN + "/api/employees";
 export const CARDS_API_URL = DOMEN + "/api/cards";
 
 //все объявления текущего клиента
-export const CLIENT_CARDS_API_URL = API_URL + "/api/client/cards";
+export const CLIENT_CARDS_API_URL = DOMEN + "/api/client/cards";
 
 export const HOUSES_API_URL = DOMEN + "/api/houses";
 
@@ -21,13 +21,13 @@ export const ADMIN_HOUSE_DESCRIPTION_API_URL = DOMEN + "/api/admin/houses/descri
 export const AUTH_USER_API_URL = DOMEN + "/api/auth/auth_user";
 
 // маршрут получения показаний всех счетчиков текущего клиента
-export const AUTH_METERS_LIST_API_URL = API_URL + "/api/client_meters";
+export const AUTH_METERS_LIST_API_URL = DOMEN + "/api/client_meters";
 
 // маршрут получения показаний всех счетчиков текущего клиента
-export const AUTH_METERS_API_URL = API_URL + "/api/client_meters/values";
+export const AUTH_METERS_API_URL = DOMEN + "/api/client_meters/values";
 
 // маршрут получения показаний всех счетчиков всех клиентов
-export const ALL_METERS_VALUES_API_URL = API_URL + "/api/admin/meters/values";
+export const ALL_METERS_VALUES_API_URL = DOMEN + "/api/admin/meters/values";
 
 //маршрут передачи показаний счетчика методом POST
 export const METER_VALUE_API_URL = DOMEN + "/api/meters";
