7e8492ab9dc3200e4d41aa0191541ea197a15670
Petrov <kostyapetrov12rus@gmail.com>
2023-01-30 16:54:10 +0300

    Api по работе с заявками добавлена проверка рабочего дня

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\TimetableController;
 use App\Http\Controllers\Auth\CodeCheckController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
@@ -101,3 +102,6 @@ Route::prefix('admin')->group(function () {
 Route::post('password/email', ForgotPasswordController::class);
 Route::post('password/code/check', CodeCheckController::class);
 Route::post('password/reset', ResetPasswordController::class);
+
+//
+Route::apiResource('timetable', TimetableController::class);
