154d6a8d0dfa8a4aff16fb702833184cd3110fa9
Petrov <kostyapetrov12rus@gmail.com>
2023-01-31 16:55:49 +0300

    Api по смене пароля, исправления

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,20 +6,21 @@ use App\Http\Controllers\ApiController;
 use App\Models\PasswordReset;
 use Illuminate\Foundation\Auth\User;
 use App\Http\Requests\Auth\ResetPasswordRequest;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
 
 class ResetPasswordController extends ApiController
 {
     public function __invoke(ResetPasswordRequest $request)
     {
-        $passwordReset = PasswordReset::firstWhere('code', $request->code);
+        $passwordReset = PasswordReset::firstWhere('code', Cookie::get('code'));
         if ($passwordReset->isExpire()) {
             return $this->jsonResponse(null, trans('passwords.code_is_expire'), 422);
         }
         $user = User::firstWhere('email', $passwordReset->email);
         $user->password = Hash::make($request->get('password'));
         $user->save();
-        PasswordReset::where('code', $request->code)->delete();
+        PasswordReset::where('code', Cookie::get('code'))->delete();
         return $this->jsonResponse(null, trans('site.password_has_been_successfully_reset'), 200);
     }
 }
