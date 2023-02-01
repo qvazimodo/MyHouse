2374abb718c700bbe0c10272d0b51b8dd78c8204
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-02-01 19:18:39 +0300

    Привёл ответ, возвращаемый HouseController@show, к стандартному виду.

--- app/Http/Controllers/API/HouseController.php
+++ app/Http/Controllers/API/HouseController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\StreetResource;
 use App\Models\House;
 use App\Models\Street;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class HouseController extends Controller
@@ -22,16 +23,15 @@ class HouseController extends Controller
 
     }
 
-    public function show($street_id, $house_number_id)
+    public function show($street_id, $house_number_id):JsonResponse
     {
-//        return HouseNumberStreet::where('street_id', $street_id)
-//            ->where('house_number_id', $house_number_id)
-//            ->join('houses', 'house_number_street_id', '=', 'house_number_street_id', 'left')
-//            ->get();
-        return House:: with('houseDescription')
+        $house =  House:: with('houseDescription')
             ->join('house_number_street','houses.house_number_street_id', '=', 'house_number_street.id' )
             ->where('house_number_street.street_id', $street_id)
             ->where('house_number_street.house_number_id', $house_number_id)
             ->get();
+
+        return response()->json(['status'=>'ok', 'data'=>$house], 200);
     }
+
 }
