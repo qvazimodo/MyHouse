906ec53a2a0e380b75465e229fe29f82b80353c4
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-02-01 19:37:50 +0300

    Создан метод HouseController@index.

--- app/Http/Controllers/API/HouseController.php
+++ app/Http/Controllers/API/HouseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\HouseCollection;
 use App\Http\Resources\StreetResource;
 use App\Models\House;
 use App\Models\Street;
@@ -11,16 +12,13 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class HouseController extends Controller
 {
-    public function index(): ResourceCollection
+    public function index():JsonResponse
     {
-        /*        return HouseResource::collection(House::with('houseDescription', 'apartments')
-                    ->join('house_number_street', 'house_number_street.id', '=', 'houses.house_number_street_id')
-                    ->join('streets', 'streets.id', '=', 'house_number_street.street_id')
-                    ->join('house_numbers', 'house_numbers.id', '=', 'house_number_street.house_number_id')
-                    ->paginate(5));*/
+        $houses =  House:: with('houseDescription')
+            ->join('house_number_street','houses.house_number_street_id', '=', 'house_number_street.id' )
+            ->get();
 
-        return StreetResource::collection(Street::with('houseNumbers')->get());
-
+        return response()->json(['status'=>'ok', 'houses'=>new HouseCollection($houses)], 200);
     }
 
     public function show($street_id, $house_number_id):JsonResponse
