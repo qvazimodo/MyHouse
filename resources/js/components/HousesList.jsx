f9f0d491bda96260a8749f8b346d18fe40543245
Anna <anna.mironova89@yandex.ru>
2023-02-03 14:14:49 +0300

    Добавила preloader

--- resources/js/components/HousesList.jsx
+++ resources/js/components/HousesList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ConfigProvider, List, theme, Carousel } from 'antd';
+import {ConfigProvider, List, theme, Carousel, Spin } from 'antd';
 import style from "../../css/houses_list.css";
 import {HOUSES_API_URL} from "../helpers/API";
 
@@ -9,6 +9,7 @@ class HousesList extends React.Component {
     state = {
         houses: [],
         images: ["https://fikiwiki.com/uploads/posts/2022-02/1644985951_20-fikiwiki-com-p-kartinki-mnogoetazhnikh-domov-21.jpg", "https://pro-dachnikov.com/uploads/posts/2021-07/1627434123_7-p-krasivie-mnogoetazhnie-doma-foto-7.jpg", "http://www.project.bulgaria-burgas.ru/multi-storey/multi-storey_59/multi-storey_59_3.jpg"],
+        loading: true,
     }
 
     componentDidMount() {
@@ -16,18 +17,21 @@ class HousesList extends React.Component {
         fetch(HOUSES_API_URL)
             .then(res => res.json())
             .then((data) => {
-                this.setState({houses: data.data});
+                this.setState({houses: data.data, loading: false});
             })
     }
 
     render() {
         return (
             <>
-                <h2 className="title_name" style={{style}}>Мы обслуживаем дома</h2>
+                <h2 className="title_name house-title" style={{style}}>Мы обслуживаем дома</h2>
                 <ConfigProvider
                     theme={{ algorithm: theme.darkAlgorithm,
                     }}
                 >
+                    {this.state.loading ? (<Spin tip="Идет загрузка..." size="large">
+                        <div className="content" />
+                    </Spin>) : (
                     <List
                         itemLayout="vertical"
                         size="large"
@@ -58,7 +62,7 @@ class HousesList extends React.Component {
                                 "К нашей управляющей компании они присоединились " + item.house_description.service_start_date.slice(0, 4) + " году."}
                             </List.Item>
                         )}
-                    />
+                    />)}
                 </ConfigProvider>
             </>
         )
