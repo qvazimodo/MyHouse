ca97d8f7e9c12a7e75d9a160129d719852965ff8
alextern0 <86599134+alextern0@users.noreply.github.com>
2023-02-04 19:39:47 +0300

    создал NewsController

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+
+    }
+    public function show()
+    {
+
+    }
+}
