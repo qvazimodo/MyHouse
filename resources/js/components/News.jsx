10a9ecb608c4787be2f22d9cebad3fa3db96b322
alextern0 <86599134+alextern0@users.noreply.github.com>
2023-02-05 15:36:03 +0300

    Update News.jsx

--- resources/js/components/News.jsx
+++ resources/js/components/News.jsx
@@ -1,23 +1,15 @@
 import { Avatar, List, Space } from 'antd';
 import React from 'react';
 import style from "../../css/news.css";
+
 const data = Array.from({
     length: 23,
 }).map((_, i) => ({
-    href: 'https://ant.design',
     title: `ant design part ${i}`,
-    avatar: 'https://joeschmoe.io/api/v1/random',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
     content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        'We supply a series of design principles, practical patterns and high quality design resources prototypes beautifully and efficiently.',
 }));
-const IconText = ({ icon, text }) => (
-    <Space>
-        {React.createElement(icon)}
-        {text}
-    </Space>
-);
+
 const News = () => (
     <>
     <h2 className="title_name">Новости</h2>
@@ -34,18 +26,9 @@ const News = () => (
         renderItem={(item) => (
             <List.Item
                 key={item.title}
-                extra={
-                    <img
-                        width={272}
-                        alt="logo"
-                        src="/images/mh.png"
-                    />
-                }
             >
                 <List.Item.Meta
-                    avatar={<Avatar src={item.avatar} />}
                     title={<a href={item.href}>{item.title}</a>}
-                    description={item.description}
                 />
                 {item.content}
             </List.Item>
