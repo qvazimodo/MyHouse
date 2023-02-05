1b598500206243501c2a86d3a823d7dc3c382c31
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-02-05 18:07:36 +0300

    В таблицу meters добавил столбец house_id для хранения показаний общедомовых приборов учёта.

--- database/migrations/2022_12_25_151653_create_meters_table_with_fk_client_id.php
+++ database/migrations/2022_12_25_151653_create_meters_table_with_fk_client_id.php
@@ -15,21 +15,15 @@ return new class extends Migration
     {
         Schema::create('meters', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('client_id')->nullable(false)->comment('id клиента');
-            $table->enum('type', ['горячая вода', 'холодная вода', 'электричество', 'тепловая энергия', 'газ'])
+            $table->unsignedBigInteger('client_id')->nullable()->comment('id клиента');
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->enum('type', ['горячая вода', 'холодная вода', 'электроэнергия', 'тепловая энергия', 'газ'])
                 ->default('горячая вода')
                 ->comment('тип счетчика');
             $table->integer('number')->default(0)->comment('заводской номер счетчика');
             $table->timestamps();
-
-
-        });
-
-        Schema::table('meters', function (Blueprint $table) {
-            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
         });
         }
-
 
 
     /**
---
+++ database/migrations/2023_01_02_143020_alter_table_meters_add_fk_house_id.php.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('meters', function (Blueprint $table) {
+            $table->unsignedBigInteger('house_id')->nullable()->comment('id дома');
+            $table->foreign('house_id')->references('id')->on('houses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('meters', function (Blueprint $table) {
+            $table->dropForeign(['house_id']);
+        });
+    }
+};
