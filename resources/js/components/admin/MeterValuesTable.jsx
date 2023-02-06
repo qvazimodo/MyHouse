587993045ee8b6e5f5816a1c86ee6b33c03a7dd2
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-02-06 18:50:08 +0300

    Вывел показания счётчика в строку.

--- resources/js/components/admin/MeterValuesTable.jsx
+++ resources/js/components/admin/MeterValuesTable.jsx
@@ -1,19 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector, } from "react-redux";
-import { Col, Row, Tag } from "antd";
+import { Badge, Card, Col, Divider, Row, Tag } from "antd";
 import { fetchMeterValues } from "../../features/house/houseSlice";
 
 export const MeterValuesTable = () => {
     let { houseAddressId, meterId } = useParams()
     const houseAddress = useSelector( state => state.house.addresses[houseAddressId] )
     const currentMeter = useSelector( state => state.house.currentMeter )
-    console.log( currentMeter )
+    const currentMeterValues = useSelector( state => state.house.currentMeterValues )
+    console.log( currentMeter, currentMeterValues )
     const dispatch = useDispatch()
 
     useEffect( () => {
         dispatch( fetchMeterValues( currentMeter.id ) )
     }, [ currentMeter ] );
+
+    const tablist = Object.keys( currentMeterValues ).map( key => {
+        return {
+            key: key,
+            tab: key
+        }
+    } )
+    console.log( tablist )
+    const [ activeTabKey, setActiveTabKey ] = useState( '2020' );
+    const onTabChange = ( key ) => {
+        setActiveTabKey( key );
+    };
+
+    const contentList = {}
+    Object.keys( currentMeterValues ).forEach( key => {
+
+            contentList[key] = (
+                <div>
+                    { currentMeterValues[key].map( month => {
+                        return (
+                            <div>{ month[name] }</div>
+                        )
+                    } ) }
+                </div>)
+
+        }
+    )
+    console.log( contentList )
 
     return (
         <div className={ 'w-full ' }>
@@ -26,10 +55,46 @@ export const MeterValuesTable = () => {
                     <Tag className={ 'p-3' } color="#3b5999">Вид энергоресурса:</Tag>
                     <span> { currentMeter.type }</span>
                 </Col>
-                <Col span={4} offset={2} className={ 'flex justify-between items-center' }>
+                <Col span={ 4 } offset={ 2 } className={ 'flex justify-between items-center' }>
                     <Tag className={ 'p-3' } color="#55acee">Номер счётчика:</Tag>
                     <span> { currentMeter.number }</span>
                 </Col>
+            </Row>
+            <Row>
+
+{/*                <Card
+                    style={ {
+                        width: '100%',
+                    } }
+                    tabList={ tablist }
+                    activeTabKey={ activeTabKey }
+                    onTabChange={ onTabChange }
+                >
+                    { contentList[activeTabKey] }
+                </Card>*/}
+                <Card
+                    style={{
+                        width: '100%',
+                    }}
+                >
+                    {Object.keys( currentMeterValues ).map( key => {
+                        return (
+                            <div className={'flex justify-between w-full'}>
+                                <div className={'align-self-center pr-5 text-2xl'}>{key}</div>
+                                {currentMeterValues[key].map(month=>{
+                                console.log(month.value)
+                                return(
+                                    <Col span={2} >
+                                        <Divider/>
+                                        <Badge className={'text-md'}>{ month.name }</Badge>
+                                        <div className={'text-lg'}>{ month.value }</div>
+
+                                    </Col>
+                                )
+                            })}</div>
+                        )
+                    })}
+                </Card>
             </Row>
 
 
