5994007fc76510a2e1f6fac769ad3a9ffbc0e868
Konstantin <kostyapetrov12rus@gmail.com>
2023-02-06 19:20:13 +0300

    APi по вызову работников, исправления

--- app/Repositories/TimetableRepository.php
+++ app/Repositories/TimetableRepository.php
@@ -42,8 +42,6 @@ class TimetableRepository
         return Employee::where('held_position', '=', $profession)
             ->join('employee_serviced_address', 'employees.id', '=', 'employee_serviced_address.employee_id')
             ->join('house_number_street', 'employee_serviced_address.house_number_street_id', '=', 'house_number_street.id')
-            /*            ->join('timetables', 'employees.id', '=', 'timetables.employer_id')
-                        ->where('timetables.date', '=', $mysqlDate)*/
             ->where('house_number_street.street_id', '=', $this->clientsAddressIds["street_id"])
             ->where('house_number_street.house_number_id', '=', $this->clientsAddressIds["house_id"])
             ->get();
@@ -56,17 +54,23 @@ class TimetableRepository
         $plucked = $employee->pluck('employee_id');
         foreach ($plucked->all() as $employee) {
             $array = [];
+            var_dump(Timetable::where('employer_id', $employee)
+                ->where('date', $mysqlDate)
+                ->get('time_window_id')
+                ->toArray());
             foreach (Timetable::where('employer_id', $employee)
                          ->where('date', $mysqlDate)
                          ->get('time_window_id')
                          ->toArray() as $item) {
                 $array[] = $item['time_window_id'];
             }
-            $array = array_diff($this->timeWindows, $array);
+            $array = (object)array_diff($this->timeWindows, $array);
             $name = User::where('id', $employee)
-                ->get(['name', 'last_name']);
+                ->get(['name', 'last_name'])
+                ->toArray();
             $name[] = $array;
-            $freeTimes[$employee] = $name;
+
+            $freeTimes[$employee] = (array)$name;
         }
         return $freeTimes;
     }
