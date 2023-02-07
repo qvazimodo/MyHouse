425497fce48981be0e88730399e3abf97ff39598
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-02-07 20:10:03 +0300

    В таблицу meters добавил столбцы класс точности, дата следующей поверки и дата изготовления. Исправлен сидер администраторов (теперь засеивается корректное имя).

--- database/seeders/EmployeeAdminstratorSeeder.php
+++ database/seeders/EmployeeAdminstratorSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\Employee;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Faker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class EmployeeAdminstratorSeeder extends Seeder
 {
@@ -17,9 +20,26 @@ class EmployeeAdminstratorSeeder extends Seeder
      */
     public function run()
     {
+        $patronymic = [
+            'Иванович', 'Петрович', 'Сергеевич', 'Тимофеевич', 'Алексеевич', 'Александрович'
+        ];
+        $faker = Faker\Factory::create('ru_Ru');
+
         foreach ($this->admintstrationPositions as $position) {
             $employee=new Employee();
-            $user = User::factory()->make(['is_admin' => true])->save();
+            $user = User::factory()->make(
+                [
+                    'name' => $faker->firstName('male'),
+                    'patronymic' => $patronymic[array_rand($patronymic)],
+                    'last_name' => $faker->lastName('male'),
+                    'birth_date' => $faker->dateTimeBetween('-50 years', '-20 years'),
+                    'phone' => fake()->unique()->phoneNumber(),
+                    'email' => fake()->unique()->safeEmail(),
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('123'), // password
+                    'remember_token' => Str::random(10),
+                    'is_admin' => '1',
+                    ])->save();
             $employee->user()->associate($user);
             $employee['held_position'] = $position;
 
