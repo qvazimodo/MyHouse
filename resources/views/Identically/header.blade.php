8316f1a9482ea3675d904e5c0bcc7e99fe7fcdfb
alextern0 <alexandr.ternovsky@gmail.com>
2023-02-11 13:37:34 +0300

    поменял формат картинок

--- resources/views/Identically/header.blade.php
+++ resources/views/Identically/header.blade.php
@@ -2,11 +2,11 @@
     <h1 class="logo-header">My House - управляющая компания</h1>
     <h2 class="quote-header">Ваше доверие наш профессионализм</h2><br/>
     <div class="sticker">
-        <img src="/Images/sticker.png" alt="photo"/>
+        <img src="/Images/sticker.webp" alt="photo"/>
         <div class="name_street">Наб. реки Фонтанки 10-15</div>
     </div>
     <div class="phone">
-        <img src="/Images/phone.png" alt="photo"/>
+        <img src="/Images/phone.webp" alt="photo"/>
         <div class="num"> 8 (812) 123-45-67</div>
     </div>
 </div>
