09ac1ab57d610ff20a9661d4246e9d5172aaee1e
Konstantin <kostyapetrov12rus@gmail.com>
2023-02-12 17:54:23 +0300

    изменения в форме с кодом

--- resources/views/emails/send-code-reset-password.blade.php
+++ resources/views/emails/send-code-reset-password.blade.php
@@ -1,10 +1,4 @@
 @component('mail::message')
-    <h1>We have received your request to reset your account password</h1>
-    <p>You can use the following code to recover your account:</p>
-
-    @component('mail::panel')
-        {{ $code }}
-    @endcomponent
-
-    <p>The allowed duration of the code is one hour from the time the message was sent</p>
+    <h1>Код для сброса пароля:</h1>
+            {{ $code }}
 @endcomponent
